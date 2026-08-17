4a6cba95098a66eb21a8ddb13a34c599fba1ed80
Morare Bokgoshi <lesbokgosh@gmail.com>
2026-08-17 20:47:26 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/digital_logbook/',
+  base: '/digital_logbook/frontend/',
   
   plugins: [react(), tailwindcss()],
 
