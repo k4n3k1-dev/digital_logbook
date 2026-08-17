cdc86f66b91a4ece060ed000e05c01e25da61afe
Morare Bokgoshi <lesbokgosh@gmail.com>
2026-08-17 20:51:50 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/digital_logbook/frontend/',
+  base: '/digital_logbook/',
   
   plugins: [react(), tailwindcss()],
 
