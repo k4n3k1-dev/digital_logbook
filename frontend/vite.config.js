cc8c1152b038e9979de201fa1cde4be7efc8eac5
Morare Bokgoshi <lesbokgosh@gmail.com>
2026-08-17 20:41:49 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,16 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/digital_logbook/',
+  
   plugins: [react(), tailwindcss()],
+
   server: {
     host: '0.0.0.0',
     port: 8443,
     strictPort: true,
   },
+
   preview: {
     host: '0.0.0.0',
     port: 8443,
